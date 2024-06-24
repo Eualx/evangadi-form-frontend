04b045e774cfd5d9c75b9536d1cb7b4bb184750a
eualx <eualx14j@gmail.com>
2024-06-25 00:19:56 +0300

    last

--- src/pages/Axios/axiosConfig.jsx
+++ src/pages/Axios/axiosConfig.jsx
@@ -3,10 +3,10 @@ import axios from "axios"
 const axiosBase = axios.create({
 
 // local host 
-    baseURL:'http://localhost:5000/api'
+    // baseURL:'http://localhost:5000/api'
 
     // web host
-//    baseURL:'https://evangadi-forum-backend-z3tu.onrender.com/api'
+ baseURL:'https://evangadi-forum-backend-z3tu.onrender.com/api'
 })
 export default axiosBase
 
